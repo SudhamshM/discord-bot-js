101aa421bfd7f940f6c61065dde0f8b80f3336c3
Sudhamsh Mondrati <sudhamshmondrati@gmail.com>
2023-04-06 14:23:36 -0400

    updated index again

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.on('messageCreate', (msg) =>
+client.on(Events.MessageCreate, (msg) =>
 {
 	if (msg.author.bot)
 		return;
