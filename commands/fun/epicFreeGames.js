b2912c2d87188b26347818037aea2c25ff893626
Sudhamsh Mondrati <sudhamshmondrati@gmail.com>
2023-04-06 14:08:33 -0400

    added epic games free api to feature

---
+++ commands/fun/epicFreeGames.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+            .setName('epic')
+            .setDescription('See currently free games on Epic Games'),
+        async execute(interaction)
+        {
+            fetch('https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions')
+            .then((response) => response.json())
+            .then((data) => 
+            {
+                let gamesList = processAPI(data);
+                let replySendable = "";
+                gamesList.forEach(game => replySendable += game.title);
+                interaction.reply(replySendable).then(() => console.log("sent games")).catch(err => console.log(err));
+            })
+            .catch(err => console.error(err));
+
+            console.log(apiResp)
+        }
+
+
+}
+
+function processAPI(apiResponse) 
+{
+    let gamesList = [];
+    apiResponse.data.Catalog.searchStore.elements.forEach(element => {
+        if (element.offerType === 'BASE_GAME')
+        {
+            gamesList.push(element);
+        }
+    });
+    return gamesList;
+}
