46a6cd1c2053f11c4e779c9900e18292ec6c5e2c
Sudhamsh Mondrati <sudhamshmondrati@gmail.com>
2023-04-11 14:54:13 -0400

    Revamp Epic response and use embed as response

--- commands/fun/epicFreeGames.js
+++ commands/fun/epicFreeGames.js
@@ -1,4 +1,5 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ActionRowBuilder, 
+  ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,10 +11,9 @@ module.exports = {
             .then((response) => response.json())
             .then((data) => 
             {
-                let gamesList = processAPI(data);
-                let replySendable = "**This week's Free Games!** :video_game: \n\n ";
-                gamesList.forEach(game => replySendable += game.title + "\n");
-                interaction.reply({embeds: embedToSend, components: components})
+              let [embedToSend, components] = processAPI(data);
+              //console.log(embedToSend, row);
+              interaction.reply({embeds: [embedToSend], components: components.components})
             })
             .catch(err => console.error(err));
         }
@@ -24,6 +24,16 @@ module.exports = {
 function processAPI(apiResponse) 
 {
     let gamesList = [];
+    let embedFinal = new EmbedBuilder();
+    let componentsFinal = {
+      "components": [
+        {
+          "type": 1,
+          "components": [
+          ]
+        }
+      ]
+    }
     apiResponse.data.Catalog.searchStore.elements.forEach(element => {
         if (element.offerType === 'BASE_GAME')
         {
@@ -31,66 +41,40 @@ function processAPI(apiResponse)
             // checking start and end time and comparing to current time 
                 if ((new Date().getTime() <= new Date(element.promotions.promotionalOffers[0].promotionalOffers[0].endDate).getTime()) 
                 && (new Date(element.promotions.promotionalOffers[0].promotionalOffers[0].startDate).getTime() <= new Date().getTime()))
-                    gamesList.push(element);
+                    {
+                        
+                        gamesList.push(element);
+                    }
+                
         }
     });
-    return gamesList;
+    gamesList.forEach(element => {
+      const gamePageUrl = element.catalogNs.mappings[0].pageSlug;
+       embedFinal = EmbedBuilder.from(embedFinal).addFields(
+          {
+            "name": `Game:`,
+            "value": `${element.title}`
+          })
+          .setDescription("Check out this week's free games from Epic Games.")
+          .setColor(0x00ffe5)
+          .setTitle(`This week's Free Games! :video_game:`)
+          .setImage("https://prod.assets.earlygamecdn.com/images/epicfree.png?mtime=1669114612")
+          .setThumbnail("https://static-assets-prod.epicgames.com/epic-store/static/webpack/25c285e020572b4f76b770d6cca272ec.png")
+          .setFooter(
+            {
+              "text": `Offers valid for this week only.`,
+              "icon_url": `https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Yellow_exclamation_mark.svg/1024px-Yellow_exclamation_mark.svg.png`
+            })
+
+        const individualComponent = 
+        {
+          "style": 5,
+          "label": `${element.title}`,
+          "url": `https://store.epicgames.com/en-US/p/${gamePageUrl}`,
+          "disabled": false,
+          "type": 2
+        }
+        componentsFinal.components[0].components.push(individualComponent);
+    });
+    return [embedFinal, componentsFinal];
 }
-
-const components = [
-    {
-      "type": 1,
-      "components": [
-        {
-          "style": 5,
-          "label": `shapez`,
-          "url": `https://store.epicgames.com/en-US/p/pageSlug`,
-          "disabled": false,
-          "type": 2
-        },
-        {
-          "style": 5,
-          "label": `Dying Light`,
-          "url": `https://store.epicgames.com/en-US/p/dying-light`,
-          "disabled": false,
-          "type": 2
-        }
-      ]
-    }
-  ]
-
-const embedToSend = [
-    {
-      "type": "rich",
-      "title": `This week's Free Games! 🎮`,
-      "description": `Check out the free games on Epic Games for this week.`,
-      "color": 0x00ffe5,
-      "fields": [
-        {
-          "name": `Game:`,
-          "value": `Dying Light: Enhanced Edition`
-        },
-        {
-          "name": `Game:`,
-          "value": `shapez`
-        }
-      ],
-      "image": {
-        "url": `https://prod.assets.earlygamecdn.com/images/epicfree.png?mtime=1669114612`,
-        "height": 0,
-        "width": 0
-      },
-      "thumbnail": {
-        "url": `https://static-assets-prod.epicgames.com/epic-store/static/webpack/25c285e020572b4f76b770d6cca272ec.png`,
-        "height": 0,
-        "width": 0
-      },
-      "author": {
-        "name": `Lover`
-      },
-      "footer": {
-        "text": `Offers valid for this week only.`,
-        "icon_url": `https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Yellow_exclamation_mark.svg/1024px-Yellow_exclamation_mark.svg.png`
-      }
-    }
-  ]
